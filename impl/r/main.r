877edf25c240b6d94f0458a64ffec4e14a9e2b65
Pedro Binotto <pedrosantibinotto@gmail.com>
2022-05-24 00:13:45 -0300

    Atualização Gráficos

--- impl/r/main.r
+++ impl/r/main.r
@@ -6,20 +6,51 @@ path_java <- './java_results.csv'
 
 content <- read.csv(path_java)
 
-qplot(test,time_elapsed,data=content)
+qplot(Dimensao,Tempo,data=content,
+    main = "Relação Elementos/Tempo - Java",
+    xlab = "Quantidade de Elementos",
+    ylab = "Tempo de Processamento (ms)", 
+    geom = c("point", "smooth"))
 
-#Resultados da linguagem rust
-path_rust <- './rust_results.csv'
+#Resultados da linguagem c
+path_c <- './c_results.csv'
 
-content <- read.csv(path_rust)
+content <- read.csv(path_c)
 
-qplot(test,time_elapsed,data=content)
+qplot(Dimensao,Tempo,data=content,
+    main = "Relação Elementos/Tempo - C",
+    xlab = "Quantidade de Elementos",
+    ylab = "Tempo de Processamento (ms)", 
+    geom = c("point", "smooth"))
 
-#Resultados da linguagem python
-path_python <- './python_results.csv'
+#Resultados da linguagem lua
+path_lua <- './lua_results.csv'
 
-content <- read.csv(path_python)
+content <- read.csv(path_lua)
 
-qplot(test,time_elapsed,data=content)
+qplot(Dimensao,Tempo,data=content,
+    main = "Relação Elementos/Tempo - Lua",
+    xlab = "Quantidade de Elementos",
+    ylab = "Tempo de Processamento (ms)", 
+    geom = c("point", "smooth"))
 
+path_final <- './final.csv'
+
+content <- read.csv(path_final)
+
+qplot(Dimensao,Tempo,data=content,
+    main = "Relação Elementos/Tempo - Geral",
+    xlab = "Quantidade de Elementos",
+    ylab = "Tempo de Processamento (ms)", 
+    geom = c("point", "smooth"),
+    col=Linguagem)
+
+qplot(Dimensao,Tempo,data=content,
+    main = "Relação Elementos/Tempo - Geral - Escala Logarítmica",
+    xlab = "Quantidade de Elementos",
+    ylab = "Tempo de Processamento (ms)", 
+    col=Linguagem,
+    geom = c("point", "smooth")) +
+    scale_y_continuous(trans='log10') +
+    scale_x_continuous(trans='log10')
 
